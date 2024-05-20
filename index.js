070cae4547e1f26ebeeb1e008e397e18c97dea01
Yuriy Ivanenko <yuriy.ivanenko@powerhrg.com>
2024-05-20 10:48:03 -0400

    Solve palindrome using for loop
    This solution uses a for loop

--- index.js
+++ index.js
@@ -1,13 +1,31 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  console.log(`Original word: ${word}`)
+  const reversedArray = []
+  for(const letter in word){
+    reversedArray.unshift(word[letter])
+  }
+  const reversedWord =  reversedArray.reduce((acc, item) => acc+item)
+  if(word === reversedWord)
+    return true
+  else{
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  1. Take argument and save as variable AS IS
+  2. Loop through string or convert string to an array of letters
+    Loop from the end of the string or array to the front
+    Save each letter into a new string or array 
+    Check if original string and new string are identical
 */
 
 /*
   Add written explanation of your solution here
+  - We will reverse the string by looping through. Not sure yet if I can loop through
+  the string or convert to an array first. After reversing we will check to see if we 
+  match the original
 */
 
 // You can run `node index.js` to view these console logs
@@ -23,3 +41,5 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+console.log(isPalindrome('yuriy'))
